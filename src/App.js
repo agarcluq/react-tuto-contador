9d5fa9d93d6a58524cda2255a9fc8f6b6554e5d8
agarcluq <agarcluq@everis.com>
2021-04-19 08:21:48 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import Prueba from "./Prueba";
+
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("Hello word");
+
+  useEffect(() => {
+    console.log('here')
+    document.title = `You clicked ${count} times`;
+  },[count]);
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <Prueba />
+      <p>{text}</p>
+      <input onChange={ev => setText(ev.target.value)} />
+      <button onClick={() => setCount(count + 1)}>Click</button>
+
     </div>
   );
 }
